ab4e7a377a2b181810830e6e9e435d09f11b8437
cool-library-2371 <est.ariel@gmail.com>
2020-03-09 15:30:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var greeting = "";
+var greeting = " Helo, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = "${topic}";
+
+
+
